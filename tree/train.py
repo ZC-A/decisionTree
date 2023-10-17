1468080f8304e35cab61cd89bccb416c7fb2d211
nucleotide <13713754853@163.com>
2023-10-17 19:35:06 +0800

    test function

--- tree/train.py
+++ tree/train.py
@@ -2,6 +2,7 @@ import copy
 from collections import Counter
 import logging
 from config import conf
+
 from tree.structure import node
 
 
@@ -104,23 +105,24 @@ def find_best_split(train_data, feature_ids):
 def test(data, tree_node,feature_ids):
         n = len(feature_ids) - 1
         length = len(data)
-        att_vals = [data[tree_node.feature_id]]  # 获得当前特征
-        label = [data[-1]]  # 获取标签
+        att_vals = data[tree_node.feature_id]  # 获得当前特征
+        label = data[-1].strip('.')  # 获取标签
+        num=0
         if tree_node.isleaf:
             if tree_node.label==label:
-                    return 1
+                return 1
+            else:
+                return 0
         if att_vals==tree_node.feature_value :
-                tree_node=test(data, tree_node.left,feature_ids)
+                num=test(data, tree_node.left,feature_ids)
         else:
-                tree_node=test(data, tree_node.right,feature_ids)
-        return 0
-
-
-def recursive(eva_data, tree_node,feature_ids):
+                num=test(data, tree_node.right,feature_ids)
+        return num
         
-        length = len(eva_data)
-        sum=0
-        for i in range(length):
-            data = eva_data[i]
-            sum+=test(data, tree_node,feature_ids)
-        return sum
+def recursive(eva_data, tree_node,feature_ids):
+    length = len(eva_data)
+    sum=0
+    for i in range(length):
+        data = eva_data[i]
+        sum+=test(data, tree_node,feature_ids)
+    return sum
