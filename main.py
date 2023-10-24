725b052f8d4249f44f2d3374b6c33da4f18514e1
ZC-A <1483681501@qq.com>
2023-10-24 15:08:07 +0800

    aggregate file

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-from collections import deque
-from typing import List
 from load.load import load_data
 import logging
-from tree.structure import node
 from tree.evaluate import evaluate
 from tree.train import train
 from config import conf
-
-
-def levelOrder(root: [node]) -> List[List[int]]:
-    list1 = []
-    q = deque()
-    q.append(root)
-    while q:
-        level = []
-        for i in range(len(q)):
-            poping = q.popleft()
-            if poping:
-                level.append(poping.feature_value)
-                q.append(poping.left)
-                q.append(poping.right)
-        if level:
-            list1.append(level)
-    return list1
 
 
 if __name__ == '__main__':
