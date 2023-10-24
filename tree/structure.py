75b50a44d24e5444417f2cf3889d4ae67d74c842
ZC-A <1483681501@qq.com>
2023-10-24 12:57:51 +0800

    change structure

--- tree/structure.py
+++ tree/structure.py
@@ -2,6 +2,7 @@ class node():
     def __init__(self) -> None:
         self.feature_id = -1
         self.feature_value = None
+        self.feature_type = None
         self.left = None
         self.right = None
         self.label = None
