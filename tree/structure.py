24a5d505da3912da572ebbcc25e2c5d59bfd107f
ZC-A <1483681501@qq.com>
2023-10-25 11:14:33 +0800

    change strucutre

--- tree/structure.py
+++ tree/structure.py
@@ -10,40 +10,45 @@ class node():
         self.right = None
         self.label = None
         self.isleaf = False
-    
+
 
 header = conf.get('header')
 
 
-def print_tree(current_node:node, nameattr='feature_value', left_child='left', right_child='right', indent='', last='updown'):
-
-    if hasattr(current_node, nameattr) is not None:
-        name = lambda node: getattr(node, nameattr)
+def print_tree(current_node: node, nameattr='feature_value', left_child='left', right_child='right', indent='', last='updown'):
 
     up = getattr(current_node, left_child)
     down = getattr(current_node, right_child)
 
     if up is not None:
         next_last = 'up'
-        next_indent = '{0}{1}{2}'.format(indent, ' ' if 'up' in last else '|', ' ' * len(str("["+header[current_node.feature_id]+":"+str(current_node.feature_value)+"]")))
+        next_indent = '{0}{1}{2}'.format(indent, ' ' if 'up' in last else '|', ' ' * len(str("[" + header[current_node.feature_id] + ":" + str(current_node.feature_value) + "]")))
         print_tree(up, nameattr, left_child, right_child, next_indent, next_last)
 
-    if last == 'up': start_shape = '┌'
-    elif last == 'down': start_shape = '└'
-    elif last == 'updown': start_shape = ' '
-    else: start_shape = '├'
+    if last == 'up':
+        start_shape = '┌'
+    elif last == 'down':
+        start_shape = '└'
+    elif last == 'updown':
+        start_shape = ' '
+    else:
+        start_shape = '├'
 
-    if up is not None and down is not None: end_shape = '┤'
-    elif up: end_shape = '┘'
-    elif down: end_shape = '┐'
-    else: end_shape = ''
+    if up is not None and down is not None:
+        end_shape = '┤'
+    elif up:
+        end_shape = '┘'
+    elif down:
+        end_shape = '┐'
+    else:
+        end_shape = ''
 
     if current_node.feature_value is None:
-        print('{0}{1}{2}{3}'.format(indent, start_shape, "["+current_node.label+"]", end_shape))
+        print('{0}{1}{2}{3}'.format(indent, start_shape, "[" + current_node.label + "]", end_shape))
     else:
-        print('{0}{1}{2}{3}'.format(indent, start_shape, "["+header[current_node.feature_id]+","+str(current_node.feature_value)+"]", end_shape))
+        print('{0}{1}{2}{3}'.format(indent, start_shape, "[" + header[current_node.feature_id] + "," + str(current_node.feature_value) + "]", end_shape))
 
     if down is not None:
         next_last = 'down'
-        next_indent = '{0}{1}{2}'.format(indent, ' ' if 'down' in last else '|', ' ' * len(str("["+header[current_node.feature_id]+":"+str(current_node.feature_value)+"]")))
+        next_indent = '{0}{1}{2}'.format(indent, ' ' if 'down' in last else '|', ' ' * len(str("[" + header[current_node.feature_id] + ":" + str(current_node.feature_value) + "]")))
         print_tree(down, nameattr, left_child, right_child, next_indent, next_last)
